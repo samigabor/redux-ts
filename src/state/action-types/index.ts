93626990a3f84a2c3f188780aabf9246a633e8ae
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-01-21 22:21:05 +0200

    add actions, action-types, reducer

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_REPOSITORIES = "search_repositories_action",
+  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success_Action",
+  SEARCH_REPOSITORIES_ERROR = "search_repositories_error_actoin",
+}
