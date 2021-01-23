72b14c375f2927b361a8947c7a5c4a324b61015c
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-01-23 09:08:35 +0200

    add RepositoriesList component

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
+  return <div>
+    <form action="" onSubmit={onSubmit}>
+      <input type="text" value={term} onChange={e => setTerm(e.target.value)}/>
+      <button>Search</button>
+    </form>
+  </div>
+}
+
+export default RepositoriesList;
