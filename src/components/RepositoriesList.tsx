878d86a348398ede44393116ae90076fb24a3582
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-02-04 21:01:26 +0200

    useSelector to display data

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import {useActions} from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const {searchRepositories} = useActions();
+  const {data, error, loading} = useSelector((state: any) => state.repositories)
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -15,6 +17,9 @@ const RepositoriesList: React.FC = () => {
       <input type="text" value={term} onChange={e => setTerm(e.target.value)}/>
       <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>Loading...</h3>}
+    {!error && !loading && data.map((name: string) => <div key={name}>{name}</div>)}
   </div>
 }
 
